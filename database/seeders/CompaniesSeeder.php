ce021faf3a9592015bd85a9bf191d007ed0e0683
Smokietr <ozgur@limeorlemon.com>
2022-05-12 01:36:28 +0300

    Companies Factory, Seeder

---
+++ database/seeders/CompaniesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Companies;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompaniesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Companies::factory()->count(50)->create();
+    }
+}
