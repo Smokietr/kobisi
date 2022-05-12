f8bde12a05f6c07666f7f9d604b7f2202472db4c
Smokietr <ozgur@limeorlemon.com>
2022-05-12 03:54:38 +0300

    Models

--- app/Models/Companies.php
+++ app/Models/Companies.php
@@ -10,4 +10,30 @@ use Laravel\Sanctum\HasApiTokens;
 class Companies extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $casts = [
+        'status' => 'boolean'
+    ];
+
+    protected $appends = [
+        'remaining'
+    ];
+
+    protected $with = [
+        'packages',
+    ];
+
+    public function packages() {
+        return $this->belongsToMany(Packages::class, 'companies_packages', 'company', 'package', 'id');
+    }
+
+    public function getRemainingAttribute() {
+        return function () {
+            return $this->packages()->first()->created_at;
+        };
+    }
+
+    public function buy() {
+        return $this->hasOne(CompaniesPayments::class, 'company', 'id');
+    }
 }
